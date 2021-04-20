b454f09e98ae19fd2ce54072db2b64b9c8f4780a
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2021-04-20 12:01:22 +0800

    node_sorter completed

---
+++ Node Sorter/node_sorter.py
@@ -0,0 +1,38 @@
+import fitz
+import pandas as pd
+import os
+from tkinter import filedialog
+
+
+current = filedialog.askdirectory(title = 'Select Folder to save files')
+#r"C:\\Users\\pasindu.s\\Desktop\\Work-Scripts\\Node Sorter\\AGRU_Nodes.csv"
+binder_file_name = filedialog.askopenfilename(title = 'Select Binder PDF(Only One file)')
+
+csv_file_name = filedialog.askopenfilename(title = 'Select CSV file with node list')
+
+#binderpdf = PyPDF2.PdfFileReader("Acid Gas Removal Unit_Node mark-up_31Mar2021.pdf",'rb')
+
+
+if current != "":
+    doc = fitz.open(binder_file_name)
+    node_df = pd.read_csv(csv_file_name)
+    node_list = node_df["Nodes"].tolist()
+    for node in node_list:
+        node_pg_count = list()
+        nodefile = fitz.open()
+        for i in range(0,doc.pageCount):
+            pg = doc[i]
+            for annot in pg.annots():
+                if((annot.type[1] == 'FreeText') & (node in annot.info["content"].split(" "))):
+                    node_pg_count.append(i)
+        for pg_num in node_pg_count:
+            nodefile.insert_pdf(doc,pg_num,pg_num,annots=True)
+            #nodefile.save()
+        #nodefile.insert_pdf(doc, extract_pg])
+        path = os.path.join(current,node)
+        os.makedirs(path)
+        pdf_path = os.path.join(path,node + '.pdf')
+        nodefile.save(pdf_path)
+        nodefile.close()
+        #with open(pdf_path,'wb') as abc:
+        #    nodefile.write(abc)
---
+++ Node Sorter/pdfcompile_script.py
@@ -0,0 +1,26 @@
+import PyPDF2
+import os
+import tkinter as tk 
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+filepath  = filedialog.askdirectory()
+
+os.chdir(filepath)
+
+listoffiles = os.listdir(filepath)
+
+listofneededfiles = [i for i in listoffiles if i.startswith('B') == True]
+pdffile = PyPDF2.PdfFileWriter()
+
+for file in listofneededfiles:
+    #pdf_file = open(file)
+    read_pdf = PyPDF2.PdfFileReader(file,'rb')
+    num_pages = read_pdf.getNumPages()
+    pdffile.addPage(read_pdf.getPage(0))
+    
+    
+with open('Compiled.pdf','wb') as abc:
+    pdffile.write(abc)
